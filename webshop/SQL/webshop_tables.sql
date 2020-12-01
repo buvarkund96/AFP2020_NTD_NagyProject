bf669e644a5b90bec6eab3b552d67b9f394c1cda
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2020-12-01 20:23:53 +0100

    felhasználók tábla létrehozása

--- webshop/SQL/webshop_tables.sql
+++ webshop/SQL/webshop_tables.sql
@@ -1,7 +1,17 @@
 
 DROP table IF EXISTS `felhasznalok`;
 
-CREATE TABLE `felhasznalok` 
+CREATE TABLE `felhasznalok` (
+  `fid` int(11) NOT NULL AUTO_INCREMENT,
+  `fnev` text COLLATE utf8_hungarian_ci NOT NULL,
+  `fjelszo` text COLLATE utf8_hungarian_ci NOT NULL,
+  `femail` text COLLATE utf8_hungarian_ci NOT NULL,
+  `flakcim` text COLLATE utf8_hungarian_ci,
+  PRIMARY KEY (`fid`),
+  UNIQUE KEY `fjelszo` (`fjelszo`(15)),
+  UNIQUE KEY `fnev` (`fnev`(15)),
+  UNIQUE KEY `femail` (`femail`(100))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
