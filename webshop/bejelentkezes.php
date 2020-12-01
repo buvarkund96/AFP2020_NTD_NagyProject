ea2f5b94384fde6cd91861408e03326423529c2b
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-01 21:47:50 +0100

    webshop/bejelentkezes.php létrehozása

---
+++ webshop/bejelentkezes.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Bejelentkezés/Regisztráció</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<?php
+include 'kapcsolat.php';
+
+?>
+<div>
+
+</div>
+<div>
+
+</div>
+</body>
+</html>
