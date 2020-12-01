dd792c60ecd17d6e854da735aa28ef044bc3ef72
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-01 23:34:50 +0100

    webshop/bejelentkezes.php - Összekapcsolása a login.php-vel

--- webshop/bejelentkezes.php
+++ webshop/bejelentkezes.php
@@ -8,8 +8,13 @@
 include 'kapcsolat.php';
 
 ?>
-<div>
+<div class="bejelentkezes">
 
+	<?php print "<form method='post' action='login.php'>";
+    print "E-mail cím: <input type='text' name='email'><br>";
+    print "Jelszó: <input type='password' name='jelszo'><br>";
+    print "<input type='submit' value='Bejelentkezés'>";
+    print "</form>"; ?>
 </div>
 <div>
 
