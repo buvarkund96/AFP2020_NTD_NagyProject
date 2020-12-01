50599e3ea7e98dbcd52ab994ae121a5405b15000
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-01 23:52:10 +0100

    webshop/login.php - Felhasználónév és jelszó ellenőrzése bejelentkezéshez

--- webshop/login.php
+++ webshop/login.php
@@ -1,3 +1,24 @@
 <?php
+session_start();
+include('kapcsolat.php');
 
+$nev			= $_REQUEST['fnev'];
+$jelszo			= $_REQUEST['fjelszo'];
+//$jelszo			= md5($jelszo);
+
+
+$sql = "SELECT fid FROM felhasznalok WHERE fnev = '$nev' AND fjelszo = '$jelszo'";
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$active = $row['active'];
+$count = mysqli_num_rows($result);
+		
+if($count == 1){
+    $_SESSION['SESS_FID'] = $nev;	
+}
+else{
+	$error = "Your Login Name or Password is invalid";
+}
+
+header("Location: bejelentkezes.php");
 ?>
