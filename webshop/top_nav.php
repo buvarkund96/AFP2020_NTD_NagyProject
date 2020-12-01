d40a7a0a134271c8544ae54238be7e98f7e7e6b2
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-02 00:38:53 +0100

    webshop/top_nav.php - Fejléc elkészítése, Bejelentkezési oldal linkjének hozzá csatolása

---
+++ webshop/top_nav.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/nav_bar.css" type="text/css">
+<div class="cim">
+    <h1 class="cimSor"> AFP_NTD Webshop <h1>
+</div>
+<div class="topnav">
+<a href="" style="border-left:2px solid black">---</a>
+<a href="">Termékek</a>
+<a href="">Impresszum</a>
+<a href="">Kapcsolat</a>
+<a href="bejelentkezes.php">Bejelentkezés - Regisztráció</a>
+</div>
