345f566cdebff9303fb84b75da2bc16f2f2f666b
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-01 23:55:11 +0100

    webshop/bejelentkezes.php - Kijelentkezési lehetőség hozzáadása, bejelentkezés után

--- webshop/bejelentkezes.php
+++ webshop/bejelentkezes.php
@@ -10,12 +10,21 @@ include 'kapcsolat.php';
 ?>
 <div>
 
-	<?php print "<form method='post' action='login.php'>";
+	<?php
+	ession_start();
+	$vid=$_SESSION['SESS_FID'];
+	
+if($vid==''){		
+	print "<form method='post' action='login.php'>";
     print "<form method='post' action='login.php'>";
     print "Felhasználónév: <input type='text' name='fnev'><br>";
     print "Jelszó: <input type='password' name='fjelszo'><br>";
     print "<input type='submit' value='Bejelentkezés'>";
     print "</form>";
+  }
+  else{
+	  print "<a href='logout.php'>Kijelentkezés</a><br>";
+  }
 	?>
 </div>
 <div>
