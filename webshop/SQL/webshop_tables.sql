44907dc2ff94d7edac168703d7055eec3b1568d6
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2020-12-01 20:21:31 +0100

    szükséges táblák

---
+++ webshop/SQL/webshop_tables.sql
@@ -0,0 +1,17 @@
+
+DROP table IF EXISTS `felhasznalok`;
+
+CREATE TABLE `felhasznalok` 
+
+
+
+DROP table IF EXISTS `termekek`;
+
+CREATE TABLE `termekek`
+
+
+
+DROP table IF EXISTS `kosar`;
+
+CREATE TABLE `kosar` 
+
