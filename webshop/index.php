1b727153e6053d0fc73a92415e26cc9062fe7938
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2020-12-02 00:10:22 +0100

    index oldal létrehozása

---
+++ webshop/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>NTD Webshop</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<?php
+include 'kapcsolat.php';
+
+?>
+<div class="navBar">
+    <?php include_once 'top_nav.php'; ?>
+</div>
+<div class="promo">
+    <?php include 'promoTermekek.php'; ?>
+</div>
+</body>
+</html>
