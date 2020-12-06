b442b8f3ba028d9260334b57d43b3678cdfef843
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2020-12-06 07:34:52 +0100

    uj termek hozzaado script

---
+++ webshop/newTermek.php
@@ -0,0 +1,21 @@
+<?php
+include_once("../kapcsolat.php");
+if(!isset($_SESSION)){
+  session_start();
+}
+$tnev=$_REQUEST['tnev'];
+$tar=$_REQUEST['tar'];
+$tleiras=$_REQUEST['tleiras'];
+
+echo $tnev;
+echo $tar;
+echo $tleiras;
+
+$sql= "INSERT INTO `termekek`(`tnev`, `tleiras`, `tar`) VALUES ('$tnev','$tar','$tleiras')";
+if ($conn->query($sql) === TRUE) {
+    echo "Sikeres feltöltés";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+header("Location:../termekek.php");
+?>
