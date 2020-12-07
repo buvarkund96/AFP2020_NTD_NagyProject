d5645a55f9c030511821a692f3df4700345ff983
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-07 23:28:39 +0100

    webshop/regisztracio.php - Üres mezők ellenőrzése

--- webshop/regisztracio.php
+++ webshop/regisztracio.php
@@ -9,17 +9,33 @@ $cim		= $_REQUEST['flakcim'];
 
 $emailCheck = $conn->query( "SELECT * FROM felhasznalok WHERE femail = '{$cim}' ");
 
-			if($emailCheck->num_rows > 0) {
-                echo ="Már létező emailcím!";                
-            } else {
-			$sql ="INSERT INTO felhasznalok (fnev, fjelszo, femail, flakcim) 
+if(!empty($nev) && !empty($jelszo) && !empty($cim) && !empty($email)){
+	if($emailCheck->num_rows > 0) {
+        echo ="Már létező emailcím!";                
+    } else {
+		$sql ="INSERT INTO felhasznalok (fnev, fjelszo, femail, flakcim) 
             VALUES ('{$nev}', '{$jelszo}', '{$email}', '{$cim}')";
-			$result = $conn->query($sql);
+		$result = $conn->query($sql);
             
-                if(!$sql){
-                    die("MySQL query failed!" . mysqli_error($conn));
-                } else {
-                    echo = "Sikeres regisztráció!";				
-                }
-			}
+        if(!$sql){
+            die("MySQL query failed!" . mysqli_error($conn));
+        } else {
+            echo = "Sikeres regisztráció!";				
+        }
+	}
+}
+else {
+    if(empty($nev)){
+		echo "Üres felhasználónév mező!";
+    }
+    if(empty($jelszo)){
+        echo "Üres jelszó mező!";
+    }
+    if(empty($email)){
+        echo "Üres emailcím mező!";
+    }
+    if(empty($cim)){
+        echo "Üres lakcím mező!";
+    } 
+}
 ?>
