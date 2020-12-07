4a3657a8e57b1ea82f6a7e66c92b5dd52c3f07c0
buvarkund96 <71435387+buvarkund96@users.noreply.github.com>
2020-12-07 23:07:53 +0100

    contact.php létrehozása és megírása

---
+++ webshop/contact.php
@@ -0,0 +1,26 @@
+<?php 
+  include 'top_nav.php';
+?>
+<html>
+<head>
+<link rel="stylesheet" href="css/kapcsolat.css">
+</head>
+<body>
+
+<div class="igen">
+<table class="da">
+<tr>
+   <td colspan="3"><h2>Kapcsolat</h2></td>
+   </tr>   
+ <tr><td class="image-box"><img src="./css/Névtelen.jpg" alt="Névtelen" class="center"></td>
+   <td>	<h3>Cégnév<br></h3>
+		<h4>Ügyfélszolgálat:<br>
+		Telefonszám: 06302564567<br>
+		Email cím: afp@ntdshop.hu<br>
+		Fax szám: 0656456346</h4></td>
+   <td class="image-box"><img src="./css/Névtelen.jpg" alt="Névtelen" class="center"></td>
+   </tr>
+</table>
+</div>
+</body>
+</html>
