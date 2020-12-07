345e871e91c65408f4a858e9d2443f53ba886884
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-07 23:46:49 +0100

    webshop/regisztracio.php - Felesleges feltétel vizsgálat törlése

--- webshop/regisztracio.php
+++ webshop/regisztracio.php
@@ -10,7 +10,6 @@ $cim		= $_REQUEST['flakcim'];
 $emailCheck = $conn->query( "SELECT * FROM felhasznalok WHERE femail = '{$cim}' ");
 $felhasznaloCheck = $conn->query("SELECT * FROM felhasznalok WHERE fnev = '{$nev}' ");
 
-if(!empty($nev) && !empty($jelszo) && !empty($cim) && !empty($email)){
 	if($emailCheck->num_rows > 0) {
         echo ="Már létező email cím!";                
     } else {
@@ -28,20 +27,6 @@ if(!empty($nev) && !empty($jelszo) && !empty($cim) && !empty($email)){
             echo = "Sikeres regisztráció!";				
         }
 	}
-}
-else {
-    if(empty($nev)){
-		echo "Üres felhasználónév mező!";
-    }
-    if(empty($jelszo)){
-        echo "Üres jelszó mező!";
-    }
-    if(empty($email)){
-        echo "Üres email cím mező!";
-    }
-    if(empty($cim)){
-        echo "Üres lakcím mező!";
-    } 
-}
+
 header("Location: bejelentkezes.php");   
 ?>
