8b8703ace6a3ec6cf1d724b8ae6a0d27969be829
buvarkund96 <71435387+buvarkund96@users.noreply.github.com>
2020-12-08 22:56:17 +0100

    kosarba.php módosítása

--- webshop/kosarba.php
+++ webshop/kosarba.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../kapcsolat.php");
+include_once "kapcsolat.php";
 if(!isset($_SESSION)){
   session_start();
 }
@@ -10,7 +10,7 @@ echo $fid=$_SESSION['fid'];
 $sql= "INSERT INTO `kosar`(`fid`, `tid`, `mennyiseg`,`kfizetve`) VALUES ('$fid','$tid','$mennyiseg','0')";
 if ($conn->query($sql) === TRUE) {
     echo "Sikeresen hozzáadva a kosrához! ";
-    header("location:../termekek.php");
+    header("location:termekek.php");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
