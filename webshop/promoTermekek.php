1bb2f85ce995bdc14be3ba4467dbe25e85aa44ce
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2020-12-08 22:16:08 +0100

    promo termekek + session + termLap

--- webshop/promoTermekek.php
+++ webshop/promoTermekek.php
@@ -1,26 +1,55 @@
 <?php
+if(!isset($_SESSION)){
+  session_start();
+}
+
 include 'kapcsolat.php';
 echo '<link rel="stylesheet" href="css/promoTermekek.css" type="text/css">' ;
-$sql= "SELECT tid, tnev, tleiras, kep FROM termekek where tid < 17";
+$sql= "SELECT tid, tnev, tleiras, tar, kep FROM termekek where tid < 17";
 $result = $conn->query($sql);
 echo '<div class="promo">';
+//----------------------------------------------------------------------------------------------------
 
+//$sid =$_SESSION['fid'];
+//----------------------------------------------------------------------------------------------------
 
-if ($result->num_rows > 0) {
-  // output data of each row
-   while($row = $result->fetch_assoc()) {
-   $kep=$row["kep"].".jpg";
-    echo '<div class="promo termekek">';
-        
-        echo "<img src='img/$kep'>";
-        echo "<p>Termék név: " . $row["tnev"];
-        echo "<p>Termék leírás: " . $row["tleiras"];
-        
-    echo '</div>';
-    
+if ( $_SESSION['fid'] != null) {
+  if ($result->num_rows > 0) {
+    // output data of each row
+     while($row = $result->fetch_assoc()) {
+     $kep=$row["kep"].".jpg";
+      echo '<div class="promo termekek">';
+        echo "<form method='post' action='termLap.php'>";
+          echo "<button type='submit'  name='id' value='".$row["tid"]."'><img style='width:40px; height:40px;'src='img/$kep'></button>";
+          echo "<p>Termék név: " . $row["tnev"];
+          echo "<p>Termék leírás: " . $row["tleiras"];
+          echo "<p> Termék Ára: ".$row["tar"];
+        echo "</form>";
+      echo '</div>';
+      
+    }
+  } else {
+    echo "0 results";
   }
-} else {
-  echo "0 results";
+} else{
+  if ($result->num_rows > 0) {
+  // output data of each row
+    while($row = $result->fetch_assoc()) {
+      $kep=$row["kep"].".jpg";
+      echo '<div class="promo termekek">';
+        
+        echo "<form method='post' action='termLap.php'>";
+        echo "<button type='submit'  name='id' value='".$row["tid"]."'><img style='width:40px; height:40px;' class='img' src='img/$kep'></button>";
+        echo "<p>Termék név:" .$row["tnev"];
+        echo "<p>Termék leírás: " . $row["tleiras"];
+        echo "</form>";
+        
+      echo '</div>';
+    
+    }
+  } else {
+    echo "0 results";
+  }
 }
 echo '</div>';
 
