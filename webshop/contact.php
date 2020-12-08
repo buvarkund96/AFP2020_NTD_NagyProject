b45cff64dbe66a307ec65d2b57530a3437b539a9
buvarkund96 <71435387+buvarkund96@users.noreply.github.com>
2020-12-08 01:38:49 +0100

    contact.php stílus javítás

--- webshop/contact.php
+++ webshop/contact.php
@@ -10,15 +10,15 @@
 <div class="igen">
 <table class="da">
 <tr>
-   <td colspan="3"><h2>Kapcsolat</h2></td>
+   <td colspan="3" style="vertical-align:bottom;"><h2>Kapcsolat</h2></td>
    </tr>   
- <tr><td class="image-box"><img src="./css/Névtelen.jpg" alt="Névtelen" class="center"></td>
+ <tr><td class="image-box"><img src="./img/Névtelen.jpg" alt="Névtelen" class="center"></td>
    <td>	<h3>Cégnév<br></h3>
 		<h4>Ügyfélszolgálat:<br>
 		Telefonszám: 06302564567<br>
 		Email cím: afp@ntdshop.hu<br>
 		Fax szám: 0656456346</h4></td>
-   <td class="image-box"><img src="./css/Névtelen.jpg" alt="Névtelen" class="center"></td>
+   <td class="image-box"><img src="./img/Névtelen.jpg" alt="Névtelen" class="center"></td>
    </tr>
 </table>
 </div>
