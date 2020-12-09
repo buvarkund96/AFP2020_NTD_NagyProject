81ae9d70cad5995197647387226042d0f44c3757
Hamar Dávid <dvdhamar75@gmail.com>
2020-12-09 22:25:13 +0100

    webshop/Test/loginTest.php - Login teszt elvégzése

--- webshop/Test/loginTest.php
+++ webshop/Test/loginTest.php
@@ -1,7 +1,25 @@
 <?php
+if(!isset($_SESSION)){
+    session_start();
+  }
+include('kapcsolat.php');
 
+	
+    echo $fnev = $_REQUEST['fnev'];
+    echo $fjelszo = $_REQUEST['fjelszo'];
+    echo $sql="SELECT fid FROM felhasznalok WHERE fnev='$fnev' && fjelszo='$fjelszo'";
+    $result = $conn->query($sql);
 
-
-
-
+    if ($result->num_rows > 0) {
+        // output data of each row
+         while($row = $result->fetch_assoc()) {
+         echo $row["fid"];
+         $_SESSION['fid'] = $row["fid"];
+         echo $_SESSION['fid'];
+         echo "A Teszt sikeresen lefutott, a bejelentkezés sikeres.";
+        }
+      } else {
+        echo "Hibás felhasználónév vagy jelszó";
+        echo "A teszt sikeresen lefutott, hibás felhasználónév vagy jelszó esetén.";
+      }
 ?>
